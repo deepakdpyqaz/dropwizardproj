36ee2dbeb63980b137fa21ea547542e149425b1e
ŖITESH KUMAR <riteshkumarofficial55@gmail.com>
2023-06-29 22:46:44 +0530

    Admin Rest Controller Creation

---
+++ src/main/java/com/flipkart/jedi/exceptions/GymAlreadyApprovedException.java
@@ -0,0 +1,6 @@
+package com.flipkart.jedi.exceptions;
+
+public class GymAlreadyApprovedException extends Exception{
+    @Override
+    public String getMessage(){return "Gym Already Approved";}
+}
---
+++ src/main/java/com/flipkart/jedi/exceptions/GymOwnerAlreadyApprovedException.java
@@ -0,0 +1,6 @@
+package com.flipkart.jedi.exceptions;
+
+public class GymOwnerAlreadyApprovedException extends Throwable {
+    @Override
+    public String getMessage(){return "GymOwner Already Approved";}
+}
